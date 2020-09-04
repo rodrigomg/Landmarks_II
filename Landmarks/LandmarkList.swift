05eb3822b83fcb2bcdda3ed5fbfdd261708e81aa
technosoul <rockdrigo.mtz@gmail.com>
2020-09-03 23:09:25 -0500

    section 4 complete ¬¬

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Rodrigo Martinez Garcia on 03/09/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List{
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
